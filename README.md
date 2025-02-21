739ca05c341e2b9106753ef573b674abf0b64f72
SumkaKiller <sasab1017@gmail.com>
2025-02-21 18:06:45 +0300

    Инициализация проекта ZapPaw: структура, зависимости, лицензия AGPLv3

---
+++ README.md
@@ -0,0 +1,5 @@
+# ZapPaw
+Серверная часть мессенджера, разработанная на Rust с использованием Actix-web, PostgreSQL и Redis.
+
+## Лицензия
+Этот проект распространяется под лицензией GNU Affero General Public License v3.0 или более поздней версии. См. файл [LICENSE](LICENSE) для подробностей.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+mod auth;
+mod chat;
+mod messages;
+mod media;
+mod notifications;
+
+#[tokio::main]
+async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Не удалось настроить глобальный подписчик логирования");
+
+
+    info!("Запуск серверной части ZapPaw...");
+
+    // TODO: Реализация сервера Actix-web будет добавлена позже.
 }
